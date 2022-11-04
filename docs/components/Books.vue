d34ae47a0de79cbf4192cae8a381acc6e1aed32e
lavard <1095041759@qq.com>
2022-11-04 16:52:34 +0800

    feat: init navbar components

---
+++ docs/components/Books.vue
@@ -0,0 +1,15 @@
+<!-- Books -->
+<template>
+    <div class="">
+        Books
+    </div>
+</template>
+
+<script setup>
+import {} from 'vue'
+    
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ docs/components/Resource.vue
@@ -0,0 +1,15 @@
+<!--  -->
+<template>
+    <div class="">
+     Resources   
+    </div>
+</template>
+
+<script setup>
+import {} from 'vue'
+    
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
